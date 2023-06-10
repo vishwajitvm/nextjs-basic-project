c3b04a6eb8a70e6f563fd704559c8d935a55708e
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-11 04:02:32 +0530

    setting layout for category in portfolio - sat

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,14 @@
-const Category = () => {
+import styles from './page.module.css'
+
+const Category = ( {params} ) => {
     return (
-      <div>
-        Category
+      <div className={styles.container}> 
+        <h1 className={styles.catTitle}> { params.category } </h1>
+        <div className={styles.item} >
+          <div className={styles.content} > </div>
+          <div className={styles.imgContainer} > </div>
+
+        </div>
       </div>
     )
   }
