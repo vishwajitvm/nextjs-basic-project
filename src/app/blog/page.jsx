cd225284e09061ae6a4df7d1e89e2a414794ed31
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-11 22:40:39 +0530

    fetch data from api in blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,43 +2,33 @@ import Link from 'next/link'
 import styles from './page.module.css'
 import Image from 'next/image'
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts") ;
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch data") ;
+  }
+  return res.json() ;
+}
+
+const Blog = async () => {
+  const data = await getData()
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container} >
+      { data.map(item => (
+
+      <Link href="/blog/testId" className={styles.container} key={item.id} >
         <div className={styles.imageContainer}>
           <Image src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg" alt='' width={400} height={250} className={styles.image} />
         </div>
 
         <div className={styles.content} >
-        <h1 className={styles.title}> Test </h1>
-        <h1 className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam fugiat numquam laborum animi. Recusandae officiis corporis atque ipsa quos? </h1>
+        <h1 className={styles.title}> { item.title } </h1>
+        <h1 className={styles.desc}> { item.body } </h1>
         </div>
       </Link>
+            )) }
 
-      
-      <Link href="/blog/testId" className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg" alt='' width={400} height={250} className={styles.image} />
-        </div>
-
-        <div className={styles.content} >
-        <h1 className={styles.title}> Test </h1>
-        <h1 className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam fugiat numquam laborum animi. Recusandae officiis corporis atque ipsa quos? </h1>
-        </div>
-      </Link>
-
-
-      <Link href="/blog/testId" className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg" alt='' width={400} height={250} className={styles.image} />
-        </div>
-
-        <div className={styles.content} >
-        <h1 className={styles.title}> Test </h1>
-        <h1 className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam fugiat numquam laborum animi. Recusandae officiis corporis atque ipsa quos? </h1>
-        </div>
-      </Link>
     </div>
   )
 }
