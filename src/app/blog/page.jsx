01b72a0bb7513406d3aa4c3d888490cfd903c1bf
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-11 18:24:35 +0530

    Blog pages design and design portfolio pages along  with new data to use as conentent

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,44 @@
+import Link from 'next/link'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>
-      Blog page
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg" alt='' width={400} height={250} className={styles.image} />
+        </div>
+
+        <div className={styles.content} >
+        <h1 className={styles.title}> Test </h1>
+        <h1 className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam fugiat numquam laborum animi. Recusandae officiis corporis atque ipsa quos? </h1>
+        </div>
+      </Link>
+
+      
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg" alt='' width={400} height={250} className={styles.image} />
+        </div>
+
+        <div className={styles.content} >
+        <h1 className={styles.title}> Test </h1>
+        <h1 className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam fugiat numquam laborum animi. Recusandae officiis corporis atque ipsa quos? </h1>
+        </div>
+      </Link>
+
+
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg" alt='' width={400} height={250} className={styles.image} />
+        </div>
+
+        <div className={styles.content} >
+        <h1 className={styles.title}> Test </h1>
+        <h1 className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam fugiat numquam laborum animi. Recusandae officiis corporis atque ipsa quos? </h1>
+        </div>
+      </Link>
     </div>
   )
 }
---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,71 @@
+export const items = {
+  applications: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "A creative portfolio application designed to showcase your artistic skills and projects. Whether you're a graphic designer, illustrator, or photographer, this application provides a visually stunning platform to present your work to potential clients and employers. With its sleek design and user-friendly interface, you can easily customize and organize your portfolio to make a lasting impression.",
+      image:
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "A minimalistic application focused on presenting a single product or service. It emphasizes simplicity and elegance, allowing your product to shine without distractions. With its clean layout and intuitive navigation, this application provides a seamless user experience. Showcase the unique features and benefits of your product through stunning visuals and compelling descriptions, captivating your audience and driving conversions.",
+      image:
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "An application dedicated to supporting and promoting charitable causes. This platform connects individuals, organizations, and volunteers, fostering a sense of community and collaboration. Through this application, users can learn about various charities, donate funds, and participate in volunteer opportunities. The intuitive interface and engaging content inspire users to make a positive impact and contribute to a better world.",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    }
+  ],
+  illustrations: [
+    {
+      id: 1,
+      title: "Creative Illustrations",
+      desc: "A collection of creative illustrations that push the boundaries of imagination and creativity. These visually captivating artworks bring stories and ideas to life, evoking emotions and sparking inspiration. From whimsical characters to breathtaking landscapes, these illustrations captivate viewers and leave a lasting impression. Each artwork tells a unique story, inviting viewers to embark on a visual journey filled with wonder and awe.",
+      image:
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimalist Illustrations",
+      desc: "Simplicity and elegance define this collection of minimalist illustrations. With their clean lines, subtle colors, and deliberate compositions, these artworks convey powerful messages in a minimalist style. Each illustration distills complex ideas into their essential elements, inviting viewers to contemplate and interpret the meaning behind the simplicity. These minimalist illustrations inspire a sense of calm and introspection, allowing viewers to connect with the essence of the subject matter.",
+      image:
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+    },
+    {
+      id: 3,
+      title: "Charity Illustrations",
+      desc: "Illustrations that aim to raise awareness and support for charitable causes. Each artwork in this collection serves as a visual representation of the impact and importance of various charities. Through vibrant colors, emotive characters, and thought-provoking compositions, these illustrations evoke empathy and inspire action. By conveying the stories and struggles of those in need, these illustrations aim to encourage viewers to contribute to the cause and make a difference.",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    }
+  ],
+  websites: [
+    {
+      id: 1,
+      title: "Creative Websites",
+      desc: "Immerse yourself in a world of creativity with these visually stunning websites. Designed to showcase artistic endeavors and creative businesses, these websites combine captivating visuals, engaging content, and seamless navigation. From portfolio websites for artists to creative agency showcases, these websites provide a platform to highlight unique talents and services. Discover the power of creativity and unleash the potential of your online presence with these inspiring websites.",
+      image:
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimalist Websites",
+      desc: "Experience the beauty of simplicity with these minimalist websites. Stripping away unnecessary elements, these websites focus on essential features, clean design, and intuitive user experience. Through the clever use of whitespace, typography, and minimal color palettes, these websites convey elegance and sophistication. Whether you're showcasing a product, sharing your portfolio, or running a blog, these minimalist websites offer a streamlined and impactful online presence.",
+      image:
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+    },
+    {
+      id: 3,
+      title: "Charity Websites",
+      desc: "Empower your charitable organization with a compelling online presence. These charity websites provide a platform to raise awareness, engage supporters, and facilitate donations. With their impactful visuals, inspiring stories, and user-friendly interfaces, these websites help convey the mission and impact of your cause. Create a sense of community, encourage involvement, and make a difference with these effective and well-designed charity websites.",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    }
+  ],
+};
