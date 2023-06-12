6b60113d9ea74962c7b03a5a28f839a1cfd64cb0
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-12 23:55:02 +0530

    Genral or old way to fetch data on client side - using react deafult [useState and useEffects]

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,29 @@
+"use client"
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        setErr(true);
+      }
+      const data = await res.json();
+      setData(data);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  console.log(data);
   return (
     <div className={styles.container}>
       dashboard
