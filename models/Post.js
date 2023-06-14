5ca561c0b23e3bf57ae15948f757b99f42122ebb
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-14 21:49:31 +0530

    Created a model and schema - for user and post collections

--- models/Post.js
+++ models/Post.js
@@ -1,2 +1,28 @@
 import mongoose from "mongoose";
 
+const { schema } = mongoose
+const postSchema = new schema({
+    title: {
+        type: String,
+        required: true
+    },
+    desc:{
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
+    
+} , { timestamps: true })
+
+export default mongoose.model("Post" , postSchema ) ;
